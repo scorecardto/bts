0d69b64ee1f6da13c199be6ce11899cb62ef0f0f
Adrian Casares <67245238+adriancasares@users.noreply.github.com>
2024-08-17 14:01:25 -0500

    fix malcolm coding issues

--- src/controllers/images/get.ts
+++ src/controllers/images/get.ts
@@ -1,19 +1,20 @@
-import {Request, Response} from "express";
+import { Request, Response } from "express";
 import requireAuth from "../../auth/requireAuth";
 import getFirebase from "../../firebase/getFirebase";
-import {magicBytes} from "./upload";
 
 export default async function getImage(req: Request, res: Response) {
-    const user = await requireAuth(req, res);
-    if (!user) return;
+  const user = await requireAuth(req, res);
+  if (!user) return;
 
-    const id = req.params.id;
-    const file = getFirebase().storage().bucket().file(id);
+  const id = req.params.id;
+  const file = getFirebase().storage().bucket().file(id);
 
-    if (!(await file.exists())) {
-        res.status(404).send("Image not found");
-        return;
-    } else{
-        res.contentType((await file.getMetadata())[0].contentType ?? "").send((await file.download())[0]);
-    }
+  if (!(await file.exists())) {
+    res.status(404).send("Image not found");
+    return;
+  } else {
+    res
+      .contentType((await file.getMetadata())[0].contentType ?? "")
+      .send((await file.download())[0]);
+  }
 }
